325e9804fc47c78f186cab44379a6a94a1880279
benaronow <benaronow227@gmail.com>
2025-02-16 20:56:45 -0500

    lab 3

--- src/Labs/TOC/index.tsx
+++ src/Labs/TOC/index.tsx
@@ -1,22 +1,45 @@
 import Nav from "react-bootstrap/Nav";
+import { Link, useLocation } from "react-router";
 
 export const TOC = () => {
+  const { pathname } = useLocation();
+
   return (
     <Nav variant="pills">
       <Nav.Item>
-        <Nav.Link href="#/Labs">Lab 1</Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab1"
+          id="wd-a1"
+          active={pathname.includes("Lab1")}
+        >
+          Lab 1
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab2"
+          id="wd-a2"
+          active={pathname.includes("Lab2")}
+        >
+          Lab 2
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab3"
+          id="wd-a3"
+          active={pathname.includes("Lab3")}
+        >
+          Lab 3
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#/Labs/Lab3">Lab 3</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
+        <Nav.Link as={Link} to="/Kambaz" id="wd-kambaz">
+          Kambaz
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link href="https://github.com/benaronow">My GitHub</Nav.Link>
