3bc0062075797cc455de069c8e51ed12bd6aab25
benaronow <benaronow227@gmail.com>
2025-03-13 00:05:18 -0400

    fix build error

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -59,6 +59,8 @@ export const Modules = () => {
                     dispatch(deleteModule(moduleId));
                   }}
                   editModule={(moduleId) => dispatch(editModule(moduleId))}
+                  setDeleteAssignmentId={() => {}}
+                  setDeleteModalOpen={() => {}}
                 />
               </div>
               {module.lessons && (
@@ -75,6 +77,8 @@ export const Modules = () => {
                         editModule={(moduleId) =>
                           dispatch(editModule(moduleId))
                         }
+                        setDeleteAssignmentId={() => {}}
+                        setDeleteModalOpen={() => {}}
                       />
                     </li>
                   ))}
