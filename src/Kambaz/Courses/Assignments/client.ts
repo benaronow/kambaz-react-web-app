cde14810d39e8889dd09c4d6aa43f8e811d3d663
Ben Aronow <benaronow227@MacBook-Pro.local>
2025-04-15 00:51:29 -0400

    assignments

--- src/Kambaz/Courses/Assignments/client.ts
+++ src/Kambaz/Courses/Assignments/client.ts
@@ -1,21 +1,33 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
+
 export const createDbAssignment = async (assignment: any) => {
-  const response = await axios.post(`${ASSIGNMENTS_API}`, assignment);
+  const response = await axiosWithCredentials.post(
+    `${ASSIGNMENTS_API}`,
+    assignment
+  );
   return response.data;
 };
+
 export const fetchDbAssignments = async (courseId: string) => {
-  const { data } = await axios.get(`${ASSIGNMENTS_API}/${courseId}`);
+  const { data } = await axiosWithCredentials.get(
+    `${ASSIGNMENTS_API}/${courseId}`
+  );
   return data;
 };
+
 export const deleteDbAssignment = async (assignmentId: string) => {
-  const { data } = await axios.delete(`${ASSIGNMENTS_API}/${assignmentId}`);
+  const { data } = await axiosWithCredentials.delete(
+    `${ASSIGNMENTS_API}/${assignmentId}`
+  );
   return data;
 };
+
 export const updateDbAssignment = async (assignment: any) => {
-  const { data } = await axios.put(
+  const { data } = await axiosWithCredentials.put(
     `${ASSIGNMENTS_API}/${assignment._id}`,
     assignment
   );
